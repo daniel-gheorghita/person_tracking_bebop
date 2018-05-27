88728d9323f9473ef61aae562de1b5094f6b57ea
daniel-gheorghita <daniel0392@gmail.com>
2018-05-27 09:59:27 +0200

    Add files via upload

--- src/bebop_control.cpp
+++ src/bebop_control.cpp
@@ -2,6 +2,7 @@
 #define HOVER_POINT 2
 #define TRACK_FACE 3
 #define TRACK_MARKER 4
+#define TRACK_PERSON 5
 
 #include "ros/ros.h"
 #include "std_msgs/String.h"
@@ -23,8 +24,8 @@
 ros::Publisher  vel_pub;
 
 geometry_msgs::Twist cmdT;
-int control_mode = 0; 
-int original_control_mode = 0;
+int control_mode = 0;
+int original_control = 0; 
 int useHovering = 1;
 double yawMean, yawSum;
 int yawCount = 30;
@@ -40,14 +41,22 @@ struct poseStruct {
     double yaw;
     int valid; // if error information is worth considering
 } poseError, d_poseError, last_poseError, i_poseError, dronePose, desiredPose, last_desiredPose, local_desiredPose;
-struct poseStruct integrated_facepose;
 
 void sendCmd_byError(poseStruct errorPose)
 {
     // PD controller params
-    double Kp = 0.5; 
-    double Kd = 1.2;
+    double Kp = 0.8; 
+    double Kd = 1.8;
     static poseStruct last_errorPose;
+    
+    // Apply deadzone
+//    double deadzone = 0.1;
+//    errorPose.x = (abs(errorPose.x) > deadzone) ? errorPose.x : 0;
+//    errorPose.y = (abs(errorPose.y) > deadzone) ? errorPose.y : 0;
+//    errorPose.z = (abs(errorPose.z) > deadzone) ? errorPose.z : 0;
+//    errorPose.yaw = (abs(errorPose.yaw) > deadzone) ? errorPose.yaw : 0;
+
+    
         
     // Prepare command (PD controller)
     cmdT.angular.z = Kp * errorPose.yaw + (errorPose.yaw - last_errorPose.yaw) * Kd;
@@ -55,7 +64,7 @@ void sendCmd_byError(poseStruct errorPose)
     //cmdT.linear.z = 0; // testing
     cmdT.linear.x = (errorPose.x) * Kp / 2 + (errorPose.x - last_errorPose.x) * Kd * 1.5;
 //    cmdT.linear.x = 0; // testing
-    cmdT.linear.y = errorPose.y * Kp / 2  + (errorPose.y - last_errorPose.y) * Kd;
+    cmdT.linear.y = errorPose.y * Kp / 2  + (errorPose.y - last_errorPose.y) * Kd * 1.5;
   //  cmdT.linear.y = 0; // testing
     //cmdT.linear.y = 0;
     cmdT.angular.x = cmdT.angular.y = useHovering ? 0 : 1;
@@ -118,18 +127,17 @@ void joystickCallback(const sensor_msgs::JoyConstPtr joy_msg)
 {
     double roll, pitch, yaw, gaz;
       
-//    for (int i = 0; i < 4; i++)
-//        std::cout << "Joystick Axes " << i << ": " << joy_msg->axes[i] << std::endl;
-//    for (int i = 0; i < 17; i++)
-//        std::cout << "Joystick Buttons " << i << ": " << joy_msg->buttons[i] << std::endl;
-
+    //for (int i = 0; i < 4; i++)
+    //    std::cout << "Joystick Axes " << i << ": " << joy_msg->axes[i] << std::endl;
+    //for (int i = 0; i < 17; i++)
+    //    std::cout << "Joystick Buttons " << i << ": " << joy_msg->buttons[i] << std::endl;
+    
     yaw = -joy_msg->axes[2];
     gaz = joy_msg->axes[3];
     roll = -joy_msg->axes[0];
     pitch = -joy_msg->axes[1];
     if (abs(yaw) > 0.1 || abs(gaz) > 0.1 || abs(roll) > 0.1 || abs(pitch) > 0.1)
     {
-        original_control_mode = control_mode;
         control_mode = JOYSTICK_CONTROL;
         std::cout << "Joystick Callback: MANUAL." << std::endl;
         global_roll = roll;
@@ -138,7 +146,7 @@ void joystickCallback(const sensor_msgs::JoyConstPtr joy_msg)
         global_gaz = gaz;
     }
     
-    if (joy_msg->buttons[10] > 0.5) // take off button
+    if (joy_msg->buttons[4] > 0.5)
     {
         // send take off message
         takeoff_pub.publish(std_msgs::Empty());
@@ -151,16 +159,16 @@ void joystickCallback(const sensor_msgs::JoyConstPtr joy_msg)
         std::cout << "Joystick Callback: LAND." << std::endl;
     }
     
-    if (joy_msg->buttons[11] == 1) // reset button
+    if (joy_msg->buttons[5] > 0.5)
     {
         // send take off message
         toggleState_pub.publish(std_msgs::Empty());
         std::cout << "Joystick Callback: RESET." << std::endl;
     }
-
-    if (joy_msg->buttons[14] == 1) // TODO: need to figure X button out
+    
+    if (joy_msg->buttons[0] > 0.5)
     {
-        control_mode = original_control_mode;
+        control_mode = original_control;
     }
 }
 
@@ -194,14 +202,20 @@ void odomCallback(const nav_msgs::Odometry::ConstPtr& msg)
 
 void markerPoseCallback(ar_track_alvar_msgs::AlvarMarkers req)
 {
-    if (control_mode != TRACK_MARKER) return;
+    
+    
+    if (control_mode != TRACK_MARKER && control_mode != TRACK_PERSON) return;
     double yaw = 0, x = 0, y = 0, z = 0;
     int countMarkers = 0;
-
+    //TODO
     static int emptyFrames; // if last 10 (?) frames are empty, something is wrong: LAND or HOVER
     
     if (!req.markers.empty())
     {
+        
+
+        
+        
         emptyFrames = 0;
         
         for (int i = 0; i < req.markers.size(); i++)
@@ -226,11 +240,10 @@ void markerPoseCallback(ar_track_alvar_msgs::AlvarMarkers req)
             Eigen::Vector3d marker_zAxis(0,0,1), drone_xAxis(1,0,0);
             Eigen::Matrix3d rotation; 
             Eigen::Vector3d marker_zAxis_local_frame;
-
             rotation = q.toRotationMatrix();
             marker_zAxis_local_frame = rotation * marker_zAxis;
+        
             marker_zAxis_local_frame[2] = 0;
-
             double cosineValue;
             double normValue = marker_zAxis_local_frame.norm();
             cosineValue = marker_zAxis_local_frame.dot(drone_xAxis) / normValue;
@@ -276,8 +289,10 @@ void markerPoseCallback(ar_track_alvar_msgs::AlvarMarkers req)
 
 void facePoseCallback(tf2_msgs::TFMessage face_tf)
 {
+
+    
     //std::cout << "FACE CALLBAAAAACK!!" << std::endl;
-    if (control_mode != TRACK_FACE) return;
+    if (control_mode != TRACK_FACE && control_mode != TRACK_PERSON) return;
     if (face_tf.transforms.empty()) return;
     //std::cout << "No. transforms: " << face_tf.transforms.size() << std::endl;
     double yaw = 0, x = 0, y = 0, z = 0;
@@ -285,14 +300,15 @@ void facePoseCallback(tf2_msgs::TFMessage face_tf)
 
     for (int i = 0; i < face_tf.transforms.size(); i++)
     {
-        if (strcmp(face_tf.transforms[i].child_frame_id.c_str(), "face_0") != 0) continue;
+        if (strcmp(face_tf.transforms[0].child_frame_id.c_str(), "face_0") != 0) continue;
+
 
         //std::cout << "FACE DETECTED!" << std::endl;
         x = face_tf.transforms[i].transform.translation.x;
         y = face_tf.transforms[i].transform.translation.y;
         z = face_tf.transforms[i].transform.translation.z;
         //std::cout << "FACE-> " <<" X: " << x << "; Y: " << y << "; Z: " << z << std::endl << std::endl;
-        local_desiredPose.x = z - 1.2; //TODO : make this sure
+        local_desiredPose.x = z - 1.2;
         local_desiredPose.y = -x;
         local_desiredPose.z = -y;
         
@@ -317,14 +333,8 @@ void facePoseCallback(tf2_msgs::TFMessage face_tf)
         yaw = acos(cosineValue) * marker_zAxis_local_frame[0] * marker_zAxis_local_frame[1];
         
         local_desiredPose.yaw = -yaw;
-//        sendCmd_byError(local_desiredPose);
+        sendCmd_byError(local_desiredPose);
 
-        integrated_facepose.x = 0.9*integrated_facepose.x + 0.1 * local_desiredPose.x;
-        integrated_facepose.y = 0.9*integrated_facepose.y + 0.1 * local_desiredPose.y;
-        integrated_facepose.z = 0.9*integrated_facepose.z + 0.1 * local_desiredPose.z;
-        integrated_facepose.yaw = 0.9*integrated_facepose.yaw + 0.1 * local_desiredPose.yaw;
-        std::cout << "Integrated pose -> x: " << integrated_facepose.x << " y: " << integrated_facepose.y << " z: " << integrated_facepose.z << std::endl;
-        sendCmd_byError(integrated_facepose);
     }
 
     //TODO
@@ -339,16 +349,19 @@ int main(int argc, char **argv)
         if (strcmp(argv[1], "joy") == 0)
         {
             control_mode = JOYSTICK_CONTROL;
+            original_control = JOYSTICK_CONTROL;
             std::cout << "Control mode: Joystick." << std::endl;
         }
         else if (strcmp(argv[1], "marker") == 0)
         {
-            control_mode = TRACK_MARKER;
+            control_mode = JOYSTICK_CONTROL;
+            original_control = TRACK_MARKER;
             std::cout << "Control mode: Marker." << std::endl;
         }
         else if (strcmp(argv[1], "hover") == 0)
         {
-            control_mode = HOVER_POINT;
+            control_mode = JOYSTICK_CONTROL;
+            original_control = HOVER_POINT;
 
             // Dummy point for testing
             desiredPose.x = 1;
@@ -358,13 +371,22 @@ int main(int argc, char **argv)
         }
         else if (strcmp(argv[1], "face") == 0)
         {
-            control_mode = TRACK_FACE;
+            control_mode = JOYSTICK_CONTROL;
+            original_control = TRACK_FACE;
             std::cout << "Control mode: Face tracker." << std::endl;
         }
+        
+        else if (strcmp(argv[1], "person") == 0)
+        {
+            control_mode = JOYSTICK_CONTROL;
+            original_control = TRACK_PERSON;
+            std::cout << "Control mode: Person tracker." << std::endl;
+        }
+        
         else
         {
             control_mode = JOYSTICK_CONTROL;
-
+            original_control = JOYSTICK_CONTROL;
             std::cout << "No valid control input. Joystick selected by default. " << std::endl;
             std::cout << "You can restart the node in the following modes: " << std::endl;
             std::cout << "1. Joystick: joy;\n2. Marker tracking: marker;\n3. Hover: hover;\n4. Face tracking: face. " << std::endl;
@@ -373,28 +395,30 @@ int main(int argc, char **argv)
     else
     {
         control_mode = JOYSTICK_CONTROL;
+        original_control = control_mode;
 
         std::cout << "No selected control input. Joystick selected by default." << std::endl;
         std::cout << "You can restart the node in the following modes: " << std::endl;
         std::cout << "1. Joystick: joy;\n2. Marker tracking: marker;\n3. Hover: hover;\n4. Face tracking: face. " << std::endl;
     }
-
+    
     // init node
     ros::init(argc, argv, "bebop_controller");
     ros::NodeHandle n;
 
+    ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
+
+    ros::Subscriber odom_sub = n.subscribe("ardrone/odom", 1000, odomCallback);
+    ros::Subscriber joy_sub = n.subscribe("joy", 1000, joystickCallback);
+    ros::Subscriber alvar_sub = n.subscribe("ar_pose_marker", 1000, markerPoseCallback);
+    ros::Subscriber face_sub = n.subscribe("tf",1000, facePoseCallback);
+
     vel_pub = n.advertise<geometry_msgs::Twist>(n.resolveName("cmd_vel"),1);
     takeoff_pub = n.advertise<std_msgs::Empty>(n.resolveName("ardrone/takeoff"),1);
-    land_pub = n.advertise<std_msgs::Empty>(n.resolveName("ardrone/land"),1);
+    land_pub    = n.advertise<std_msgs::Empty>(n.resolveName("ardrone/land"),1);
     toggleState_pub = n.advertise<std_msgs::Empty>(n.resolveName("ardrone/reset"),1);
 
-    ros::Subscriber joy_sub, alvar_sub, odom_sub, face_sub;
-
-    odom_sub = n.subscribe("ardrone/odom", 1000, odomCallback);
-    alvar_sub = n.subscribe("ar_pose_marker", 1000, markerPoseCallback);
-    joy_sub = n.subscribe("joy", 1000, joystickCallback);
-    face_sub = n.subscribe("tf",1000, facePoseCallback);
-
+    
     ros::Rate loop_rate(10);
         
     // run node - send control commands
@@ -435,11 +459,17 @@ int main(int argc, char **argv)
             case TRACK_FACE:
             {
                 std::cout << "Control mode: TRACK FACE." << std::endl;
+
                 break;
             }
             case HOVER_POINT:
             {
                 std::cout << "Control mode: HOVER POINT." << std::endl;                 
+                break;
+            }
+            case TRACK_PERSON:
+            {
+                std::cout << "Control mode: TRACK PERSON." << std::endl;                 
                 break;
             }
             default:
